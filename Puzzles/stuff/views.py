fb51a0b740854a416f1389568dad09e1e56d5f0d
Pranav Agarwal <pranav-agarwal800@gmail.com>
2021-05-17 18:11:55 +0530

    3rd commit

--- Puzzles/stuff/views.py
+++ Puzzles/stuff/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Items 
 
 def home(request):
     return render(request, 'stuff/home.html')
 
 def suits(request):
+    
     return render(request, 'stuff/suits.html', {'title': 'Suits'})
